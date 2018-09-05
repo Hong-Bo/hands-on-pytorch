916ee83510f6d4ccab52fece490ca7d020dc8e44
denghongbo <hongbo@univer.ai>
2018-09-05 10:23:27 +0800

    cnn completed

--- cnn/main.py
+++ cnn/main.py
@@ -4,7 +4,7 @@ import torchvision.transforms as transforms
 
 def main():
     pipe = Pipeline(
-        input_size=28 * 28, hidden_size=500, output_size=10,
+        input_size=7*7*32, output_size=10,
         data_dir='../data', batch_size=100, transform=transforms.ToTensor(),
         log_interval=50, epochs=10, load_model=True
         )
